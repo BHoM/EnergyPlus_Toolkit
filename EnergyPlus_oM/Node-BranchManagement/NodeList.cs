a52de8dc656f7f567cebcbee1fee3b73d7ffe30a
BHoMBot <BHoMBot@burohappold.com>
2022-01-06 11:11:20 +0000

    Update copyright headers to 2022

--- EnergyPlus_oM/Node-BranchManagement/NodeList.cs
+++ EnergyPlus_oM/Node-BranchManagement/NodeList.cs
@@ -1,6 +1,6 @@
 /*
  * This file is part of the Buildings and Habitats object Model (BHoM)
- * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
+ * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
  *
  * Each contributor holds copyright over their respective contributions.
  * The project versioning (Git) records all such contribution source information.
@@ -1032,4 +1032,5 @@
 //public virtual node Node500Name { get; set; } = new node;
 //}
 //}
+
 
