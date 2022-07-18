88c74be50117c416d91cd0f96956b803d1458f1f
FraserGreenroyd <fraser.greenroyd@burohappold.com>
2022-07-18 17:38:05 +0100

    Update Assembly Information

--- EnergyPlus_Adapter/Properties/AssemblyInfo.cs
+++ EnergyPlus_Adapter/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EnergyPlus_Adapter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/BHoM/EnergyPlus_Toolkit")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EnergyPlus_Adapter")]
