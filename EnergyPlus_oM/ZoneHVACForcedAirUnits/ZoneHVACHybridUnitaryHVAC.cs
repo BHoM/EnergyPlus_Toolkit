3258c124cc349f5165dfac6625a8028d96d59223
BHoMBot <BHoMBot@burohappold.com>
2023-01-04 11:35:18 +0000

    Update copyright headers to 2023

--- EnergyPlus_oM/ZoneHVACForcedAirUnits/ZoneHVACHybridUnitaryHVAC.cs
+++ EnergyPlus_oM/ZoneHVACForcedAirUnits/ZoneHVACHybridUnitaryHVAC.cs
@@ -1,6 +1,6 @@
 /*
  * This file is part of the Buildings and Habitats object Model (BHoM)
- * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
  *
  * Each contributor holds copyright over their respective contributions.
  * The project versioning (Git) records all such contribution source information.
@@ -1344,5 +1344,6 @@
 ////public virtual real Mode25MaximumSupplyAirMassFlowRateRatio { get; set; } = new real;
 ////}
 ////}
+
 
 
